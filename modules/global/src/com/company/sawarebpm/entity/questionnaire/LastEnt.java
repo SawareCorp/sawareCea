17c55d9fe179f1b5e365bc317109cfffeeee0a6a
Nomat711 <programer711@gmail.com>
2018-03-09 23:14:22 +0300

    lastEnt для теста форков

---
+++ modules/global/src/com/company/sawarebpm/entity/questionnaire/LastEnt.java
@@ -0,0 +1,12 @@
+package com.company.sawarebpm.entity.questionnaire;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+@Table(name = "SAWARECEA_LAST_ENT")
+@Entity(name = "sawarecea$LastEnt")
+public class LastEnt extends StandardEntity {
+    private static final long serialVersionUID = -8841408729831000227L;
+
+}
