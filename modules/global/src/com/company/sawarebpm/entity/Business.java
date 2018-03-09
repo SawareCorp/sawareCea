5e7e1c3004dcc7e2cf896b1e5724f45eb256102e
Aridg <telerman.bas@yandex.ru>
2018-03-09 12:52:50 +0300

    Добавлена сущность "Дело"

---
+++ modules/global/src/com/company/sawarebpm/entity/Business.java
@@ -0,0 +1,98 @@
+package com.company.sawarebpm.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+import com.haulmont.cuba.core.entity.BaseUuidEntity;
+import com.haulmont.cuba.core.entity.Versioned;
+import com.haulmont.cuba.core.entity.Updatable;
+import com.haulmont.cuba.core.entity.Creatable;
+
+@Table(name = "SAWARECEA_BUSINESS")
+@Entity(name = "sawarecea$Business")
+public class Business extends BaseUuidEntity implements Versioned, Updatable, Creatable {
+    private static final long serialVersionUID = 2119277701386595183L;
+
+    @Column(name = "UPDATE_TS")
+    protected Date updateTs;
+
+    @Column(name = "UPDATED_BY", length = 50)
+    protected String updatedBy;
+
+    @Column(name = "CREATE_TS")
+    protected Date createTs;
+
+    @Column(name = "CREATED_BY", length = 50)
+    protected String createdBy;
+
+    @Version
+    @Column(name = "VERSION", nullable = false)
+    protected Integer version;
+
+    @NotNull
+    @Column(name = "NUMBER_", nullable = false, unique = true)
+    protected Long number;
+
+    @Override
+    public void setUpdateTs(Date updateTs) {
+        this.updateTs = updateTs;
+    }
+
+    @Override
+    public Date getUpdateTs() {
+        return updateTs;
+    }
+
+    @Override
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    @Override
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    @Override
+    public void setCreateTs(Date createTs) {
+        this.createTs = createTs;
+    }
+
+    @Override
+    public Date getCreateTs() {
+        return createTs;
+    }
+
+    @Override
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    @Override
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    @Override
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    @Override
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+
+}
