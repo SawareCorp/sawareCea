692005dec8362c34e2bfa318addfc5fbd538151d
Nomat711 <programer711@gmail.com>
2018-03-09 15:12:55 +0300

    Анкета общая
    добавлены промежуточные сущности (закрытые и текущие кредиты..)

--- modules/global/src/com/company/sawarebpm/entity/questionnaire/ClosedLoans.java
+++ modules/global/src/com/company/sawarebpm/entity/questionnaire/ClosedLoans.java
@@ -1,26 +1,27 @@
-package com.company.sawarebpm.entity.directories;
+package com.company.sawarebpm.entity.questionnaire;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import com.company.sawarebpm.entity.directories.Bank;
 import java.util.Date;
+import java.util.List;
 import javax.persistence.Column;
+import javax.persistence.OneToMany;
 import javax.persistence.Version;
-import javax.validation.constraints.NotNull;
 import com.haulmont.cuba.core.entity.BaseUuidEntity;
 import com.haulmont.cuba.core.entity.Versioned;
 import com.haulmont.cuba.core.entity.Updatable;
 import com.haulmont.cuba.core.entity.Creatable;
-import com.haulmont.chile.core.annotations.NamePattern;
-import com.company.sawarebpm.entity.questionnaire.ClosedLoans;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-@NamePattern("%s|name")
-@Table(name = "SAWARECEA_BANK")
-@Entity(name = "sawarecea$Bank")
-public class Bank extends BaseUuidEntity implements Versioned, Updatable, Creatable {
-    private static final long serialVersionUID = 4648223576195200580L;
+@Table(name = "SAWARECEA_CLOSED_LOANS")
+@Entity(name = "sawarecea$ClosedLoans")
+public class ClosedLoans extends BaseUuidEntity implements Versioned, Updatable, Creatable {
+    private static final long serialVersionUID = -163503580559927257L;
 
     @Column(name = "UPDATE_TS")
     protected Date updateTs;
@@ -38,29 +39,54 @@ public class Bank extends BaseUuidEntity implements Versioned, Updatable, Creata
     @Column(name = "VERSION", nullable = false)
     protected Integer version;
 
-    @NotNull
-    @Column(name = "NAME", nullable = false, unique = true)
-    protected String name;
+    @OneToMany(mappedBy = "closedLoans")
+    protected List<Bank> bank;
 
-    @Column(name = "AGENT_PHONE")
-    protected String agentPhone;
+    @Column(name = "TYPE_OF_CREDIT")
+    protected String typeOfCredit;
 
-    @Column(name = "ADDRESS")
-    protected String address;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "CLOSING_DATE")
+    protected Date closingDate;
 
-    @Column(name = "AGENT_NAME")
-    protected String agentName;
+    @Column(name = "COMMENT_", length = 2000)
+    protected String comment;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "CLOSED_LOANS_ID")
-    protected ClosedLoans closedLoans;
+    @JoinColumn(name = "QUESTIONNAIRE_ID")
+    protected Questionnaire questionnaire;
 
-    public void setClosedLoans(ClosedLoans closedLoans) {
-        this.closedLoans = closedLoans;
+    public void setQuestionnaire(Questionnaire questionnaire) {
+        this.questionnaire = questionnaire;
     }
 
-    public ClosedLoans getClosedLoans() {
-        return closedLoans;
+    public Questionnaire getQuestionnaire() {
+        return questionnaire;
+    }
+
+
+    public void setTypeOfCredit(String typeOfCredit) {
+        this.typeOfCredit = typeOfCredit;
+    }
+
+    public String getTypeOfCredit() {
+        return typeOfCredit;
+    }
+
+    public void setClosingDate(Date closingDate) {
+        this.closingDate = closingDate;
+    }
+
+    public Date getClosingDate() {
+        return closingDate;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getComment() {
+        return comment;
     }
 
 
@@ -114,36 +140,12 @@ public class Bank extends BaseUuidEntity implements Versioned, Updatable, Creata
         return version;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setBank(List<Bank> bank) {
+        this.bank = bank;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setAgentPhone(String agentPhone) {
-        this.agentPhone = agentPhone;
-    }
-
-    public String getAgentPhone() {
-        return agentPhone;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAgentName(String agentName) {
-        this.agentName = agentName;
-    }
-
-    public String getAgentName() {
-        return agentName;
+    public List<Bank> getBank() {
+        return bank;
     }
 
 
